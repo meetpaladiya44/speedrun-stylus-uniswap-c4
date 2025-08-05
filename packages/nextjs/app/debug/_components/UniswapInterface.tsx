2bdf363e6ad0c68356cdc957e7561ecd62ed3aa3
meetpaladiya44 <meetpaladiya2178@gmail.com>
2025-08-05 10:04:35 +0530

    completed uniswap-v2  challenge, deployed contract on arb sepolia

--- packages/nextjs/app/debug/_components/UniswapInterface.tsx
+++ packages/nextjs/app/debug/_components/UniswapInterface.tsx
@@ -189,7 +189,7 @@ export default function UniswapInterface() {
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       const signer = new ethers.Wallet(privateKey, provider);
       const signerAddress = await signer.getAddress();
-      const uniswapContractAddress = "0x40b694c75b4e509aa8cbe9993a37af9546ce36e8";
+      const uniswapContractAddress = "0xc474646e0e15894a58bb109d53f27cb01b52ca5b";
       const uniswapContract = new ethers.Contract(uniswapContractAddress, IUniswapV2, signer);
       console.log("contract", uniswapContract);
       setContract(uniswapContract);
